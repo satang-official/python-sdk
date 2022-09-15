9b36208e6d31b62b85eefeb108ac91d733113566
Thaeneat <107472354+Thaeneat@users.noreply.github.com>
2022-09-15 11:43:01 +0700

    V1.0.3

--- satang_pro_sdk/__init__.py
+++ satang_pro_sdk/__init__.py
@@ -4,8 +4,8 @@ import time
 import hashlib
 import hmac
 import json
-from get_unix_time import nonce
-from all_request import get_request, post_requst, delete_request
+from satang_pro_sdk.get_unix_time import nonce
+from satang_pro_sdk.all_request import get_request, post_requst, delete_request
 from satang_pro_sdk.satangpro import Satangpro
 
 
